d69b53e4c870ae6caf10feb16f9bf7fc6e6d45bf
Dennis Kats <denniskats43@gmail.com>
2022-12-05 10:10:03 -0500

    day5 rust

---
+++ day5a/rust/src/main.rs
@@ -0,0 +1,97 @@
+use regex::Regex;
+use std::fs;
+
+const FILE_PATH: &str = "../input.txt";
+
+fn main() {
+    let Ok(file_content) = fs::read_to_string(FILE_PATH) else {
+        panic!("Can't read file {FILE_PATH}");
+    };
+
+    let [stack_part, instructions_part] =
+        file_content.split("\n\n").collect::<Vec<&str>>()[..] else {
+            panic!("Can't parse parts of the file.");
+        };
+
+    let stack_lines = parse_stack_part(stack_part);
+    let mut stacks = create_stack_from_lines(stack_lines);
+
+    let instructions = parse_instructions(instructions_part);
+
+    perform_instructions(&mut stacks, instructions);
+
+    for stack in stacks {
+        print!("{}", stack.last().unwrap());
+    }
+    println!();
+}
+
+fn parse_stack_part(stack_part: &str) -> Vec<Vec<char>> {
+    stack_part
+        .lines()
+        .map(|line| {
+            line.char_indices()
+                .skip(1)
+                .filter_map(|(index, char)| {
+                    if (index - 1) % 4 == 0 {
+                        Some(char)
+                    } else {
+                        None
+                    }
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn create_stack_from_lines(stack_lines: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let total_stacks = stack_lines[0].len();
+    let stacks_height = stack_lines.len() - 1;
+
+    let mut stacks = vec![vec![]; total_stacks];
+
+    for row in (0..stacks_height).rev() {
+        let line = &stack_lines[row];
+        for (index, char) in line.iter().enumerate() {
+            if !char.is_whitespace() {
+                stacks[index].push(*char);
+            }
+        }
+    }
+
+    return stacks;
+}
+
+fn parse_instructions(instructions_part: &str) -> Vec<Vec<usize>> {
+    let re = Regex::new(r"\d+").unwrap();
+
+    instructions_part
+        .lines()
+        .map(|line| {
+            re.find_iter(line)
+                .map(|num_match| num_match.as_str().parse().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn perform_instructions(
+    stacks: &mut Vec<Vec<char>>,
+    instructions: Vec<Vec<usize>>,
+) -> &mut Vec<Vec<char>> {
+    for instruction in instructions {
+        let remove_amount = instruction[0];
+        let from = instruction[1] - 1;
+        let to = instruction[2] - 1;
+
+        let mut removed = vec![];
+        for _ in 0..remove_amount {
+            let popped = stacks[from].pop().unwrap();
+            removed.push(popped);
+        }
+
+        stacks[to].append(&mut removed);
+    }
+
+    return stacks;
+}
